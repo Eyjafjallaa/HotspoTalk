ccb23c944bf3b5a4bdf3563621632779b68e64ca
tmdeh <yus174113@gmail.com>
2021-09-29 17:56:28 +0900

    account 라우팅

---
+++ routes/account.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.post('/signup', function(req, res, next) {
+
+});
+
+router.post('/login', (req, res, next) => {
+
+});
+
+router.get('/:id', (req, res, next) => {
+
+});
+
+router.delete('/ban', (req, res, next) => {
+
+});
+
+module.exports = router;
