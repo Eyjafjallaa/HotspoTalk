8ab730148193046732694fd249de618bf6d923d4
Eyjafjallaa <63107258+Eyjafjallaa@users.noreply.github.com>
2021-09-29 19:29:15 +0900

    Update account.js

--- routes/account.js
+++ routes/account.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../model/db');
-
+var jwt = require('jsonwebtoken')
 
 /* GET users listing. */
 router.post('/signup', function(req, res, next) {
@@ -9,7 +9,29 @@ router.post('/signup', function(req, res, next) {
 });
 
 router.post('/login', (req, res, next) => {
-
+    db.query(`SELECT PW FROM Broker WHERE ID=?`, [req.body.id], (err, result) => {
+        //console.log(result)
+        if (result[0] == undefined) {
+        res.status(401).json({});
+        return;
+      }
+      if (req.body.pw == result[0].PW) {
+        var user = {
+          sub: req.body.id,
+          type:"broker",
+          iat: new Date().getTime() / 1000
+        };
+        var token = jwt.sign(user, secret, {
+          expiresIn: "32H"
+        })
+        res.status(200).json({
+          logintoken: token,
+        });
+      }
+      else {
+        res.status(401).json({});
+      }
+    })
 });
 
 router.get('/:id', (req, res, next) => {
