5a87d01f0663440293ecbf4c39a43a21c828028d
Eyjafjallaa <63107258+Eyjafjallaa@users.noreply.github.com>
2021-10-06 17:44:15 +0900

    db changed

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,12 +1,11 @@
-const mysql = require('mysql');
-
-var pool = mysql.createConnection({
-    connectTimeout:10,
+var pool = {
     host:'10.80.161.222',
     port:'3306',
     user:'hotspotalk',
     password:'1234',
-    database : 'hotsix'
-});
+    database : 'hotsix',
+    connectionLimit : 20,
+    queueLimit: 0,
+};
 
 module.exports=pool;
