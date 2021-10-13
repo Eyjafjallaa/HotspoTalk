62b6a47a159584a520535ff04415c155c868c74e
tmdeh <yus174113@gmail.com>
2021-10-13 17:09:46 +0900

    room 라우팅

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,8 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', (req, res)=>{
+router.post('/', (req, res)=>{
     res.render('index');
 })
+
+router.get('/:roomid', (req, res) => {
+
+})
+
+router.get('/:roomid/user', (req, res) => {
+
+})
+router.get('/:roomid/member', (req, res) => {
+
+})
+router.delete('/:roomid/exit', (req, res) => {
+
+})
+
+router.put('/:roomid/edit', (req, res) => {
+
+})
+
+router.put('/:roomid/inherit', (req, res) => {
+
+})
+
+router.delete('/:roomid', (req, res) => {
+
+})
+
+router.put('/:roomid/rename')
 
 module.exports = router;
